711ffb3f338175a59b51802c76d90a93f2df2e4b
oskol <oskols@mail.ru>
2022-06-08 23:54:03 +0300

    Домашнее задание

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
 public class Main {
     public static void main(String[] args) {
-
+        task1();
+        task2();
+        task3();
     }
+
+    public static String full() {
+        String firstName = "Ivan";
+        String meddleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + ' ' + firstName + ' ' + meddleName;
+        return fullName;
+    }
+    private static void task1() {
+        String fullName = full();
+        System.out.println("Ф.И.О. сотрудника - " + fullName);
+    }
+
+    private static void task2() {
+        String fullName = full();
+        fullName = fullName.toUpperCase();
+        System.out.println("Ф.И.О. сотрудника - " + fullName);
+    }
+
+    private static void task3() {
+        String fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace('ё', 'е');
+        System.out.println("Данные ФИО сотрудника - " + fullName);
+    }
+
 }
